263eccb2ee0f234e4fe531c54636fcdeded7635f
Truong <duytruong5437@gmail.com>
2021-09-15 19:31:12 +0700

    create spotify-clone

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import SpotifyWebApi from "spotify-web-api-js";
+import {getTokenFromResponse} from './spotify'
+import Login from './Components/Login/Login';
+import {useStateValue} from './StateProvider';
+import Player from './Components/Player/Player'
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+  const [{ token }, dispatch] = useStateValue();
+  useEffect(() => {
+    const hash = getTokenFromResponse();
+    window.location.hash="";
+   let _token = hash.access_token;
+   console.log("Token", _token);
+   if(_token){
+    dispatch({
+      type: "SET_TOKEN",
+      token: _token,
+    });
+
+     spotify.setAccessToken(_token);
+     
+    spotify.getMe().then((user) => {
+      dispatch({
+        type: "SET_USER",
+        user,
+      });
+    });
+    spotify.getUserPlaylists().then(playlists=>{
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists,
+        })
+    });
+    spotify.getPlaylist("0vlv4o2JqFbavPSs8BqvUd").then((response) =>
+    dispatch({
+      type: "SET_DISCOVER_WEEKLY",
+      discover_weekly: response,
+    })
+  );
+
+  spotify.getMyTopArtists().then((response) =>
+    dispatch({
+      type: "SET_TOP_ARTISTS",
+      top_artists: response,
+    })
+  );
+
+  dispatch({
+    type: "SET_SPOTIFY",
+    spotify: spotify,
+  });
+
+
+   }
+
+  }, [token, dispatch]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        token ? (
+          <Player spotify={spotify}/>
+        ):
+        ( 
+        <Login />
+         )
+      }
+       
     </div>
   );
 }
---
+++ src/Components/Body/Body.js
@@ -0,0 +1,74 @@
+import React from "react";
+import "./Body.css";
+import { useStateValue } from "../../StateProvider";
+import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import Header from "../Header/Header";
+import SongRow from "../SongRow/SongRow";
+
+export default function Body({ spotify }) {
+  const [{ discover_weekly }, dispatch] = useStateValue();
+  const playPlaylist = (id) => {
+    spotify
+      .play({
+        context_uri: `spotify:playlist:0vlv4o2JqFbavPSs8BqvUd`,
+      })
+      .then((res) => {
+        spotify.getMyCurrentPlayingTrack().then((r) => {
+          dispatch({
+            type: "SET_ITEM",
+            item: r.item,
+          });
+          dispatch({
+            type: "SET_PLAYING",
+            playing: true,
+          });
+        });
+      });
+  };
+  const playSong = (id) => {
+    spotify
+      .play({
+        uris: [`spotify:track:${id}`],
+      })
+      .then((res) => {
+        spotify.getMyCurrentPlayingTrack().then((res) => {
+          dispatch({
+            type: "SET_ITEM",
+            item: res.item,
+          });
+          dispatch({
+            type: "SET_PLAYING",
+            playing: true,
+          });
+        });
+      });
+  };
+  return (
+    <div className="body">
+      <Header spotify={spotify} />
+      <div className="body__info">
+        <img src={discover_weekly?.images[0].url} alt="images" />
+        <div className="body__infoText">
+          <strong>PLAYLIST</strong>
+          <h2>Discover Weekly</h2>
+          <p>{discover_weekly?.description}</p>
+        </div>
+      </div>
+      <div className="body__songs">
+        <div className="body_icons">
+          <PlayCircleFilledIcon
+            className="body__shuffle"
+            onClick={playPlaylist}
+          />
+          <FavoriteIcon fontSize="large" />
+          <MoreHorizIcon />
+        </div>
+        {discover_weekly?.tracks.items.map((item) => (
+          <SongRow playSong={playSong} track={item.track} />
+        ))}
+      </div>
+    </div>
+  );
+}
